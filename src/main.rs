7a78ed30b701731cb9f93a57d6590784217d0e68
Caden Kirby <cadenkirby@outlook.com>
2022-11-07 22:55:54 -0600

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 
-const GRAVITY_FPS: f64 = 32.1741;
+// Feet per second squared
+const GRAVITY: f64 = 32.17404855643;
 
 // #[derive(Debug)]
 struct MaxInfo {angle: f64, max: f64}
@@ -16,6 +17,7 @@ fn main() {
 
     // Convert from mph to fps
     let arm_velocity: f64 = (arm_velocity * 5280.0) / (60.0 * 60.0);
+    let arm_veolocity_sq: f64 = f64::powi(arm_velocity, 2);
     
     // Determine launch angle that yields the greastest distance (x)
     let mut launch_angle: f64 = 90.0;
@@ -25,25 +27,28 @@ fn main() {
 
         let launch_angle_rad = launch_angle.to_radians();
         let launch_angle_cos = launch_angle_rad.cos();
+        let launch_angle_tan = launch_angle_rad.tan();
+        let launch_angle_cos_sq = f64::powi(launch_angle_cos, 2);
 
         let arm_height: f64 = launch_angle_cos * arm_length;
         let launch_height: f64 = body_height + arm_height;
         let mut y: f64 = launch_height;
         let mut y_peak: f64 = y;
 
-        let launch_x: f64 = launch_angle_rad.sin() * arm_length;
-        let mut x: f64 = launch_x;
-
+        let mut x: f64 = 0.0;
         while y >= 0.0 {
-            let x_squared: f64 = f64::powi(x, 2);
-            let arm_velocity_squared: f64 = f64::powi(arm_velocity, 2);
-            y = launch_height + ( x * launch_angle_rad.tan() ) - ( (GRAVITY_FPS * x_squared) / (2.0 * arm_velocity_squared * f64::powi(launch_angle_cos, 2)) );
+            let denominator: f64 = 2.0 * arm_veolocity_sq * launch_angle_cos_sq;
+            y = launch_height + x * launch_angle_tan - GRAVITY * ( f64::powi(x, 2) / denominator );
             y_peak = if y > y_peak {y} else {y_peak};
             x += 0.1;
         }
+
+        let launch_x: f64 = launch_angle_rad.sin() * arm_length;
+        let final_x: f64 = x - launch_x;
+
         // Decide if maxes are overwritten
-        x_info = if x > x_info.max { 
-            MaxInfo {angle:launch_angle, max:x} 
+        x_info = if final_x > x_info.max { 
+            MaxInfo {angle:launch_angle, max:final_x} 
         } else {
             x_info
         };
@@ -58,8 +63,8 @@ fn main() {
     }
 
     // Print the stuff
-    println!("Best Distance was {:.2}ft at an angle of {:.2} degrees", x_info.max, x_info.angle);
-    println!("Best Height was {:.2}ft at an angle of {:.2} degrees", y_info.max, y_info.angle);
+    println!("Max Distance was {:.2}ft at an angle of {:.2} degrees", x_info.max, x_info.angle);
+    println!("Max Height was {:.2}ft at an angle of {:.2} degrees", y_info.max, y_info.angle);
 }
 
 fn get_float64_input (message: String) -> f64 {
